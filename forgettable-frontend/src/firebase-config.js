4dbd3ad23f6191f736dcb50dd8fa1b023f6ed738
Harris Tomy <68502356+htom380@users.noreply.github.com>
2022-03-26 21:52:35 +1300

    Add existing firebaseConfig for CI test

---
+++ forgettable-frontend/src/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC1BtS4u1oIsbLSKA1TCzNK8f-PzfiXFOE',
+  authDomain: 'forgettable-78b96.firebaseapp.com',
+  projectId: 'forgettable-78b96',
+  storageBucket: 'forgettable-78b96.appspot.com',
+  messagingSenderId: '762262832965',
+  appId: '1:762262832965:web:25beb236f7ff93b05f602c',
+};
+
+export default firebaseConfig;
